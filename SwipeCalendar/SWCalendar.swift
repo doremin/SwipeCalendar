1ff10763bfa0ba55fab2b09f50c049e2b1f75630
Domin Kim <doremin98@gmail.com>
2023-10-31 03:49:34 +0900

    Feat: SWCalendar에 특정 날짜가 속한 주를 생성하는 기능 추가

---
+++ SwipeCalendar/SWCalendar.swift
@@ -0,0 +1,43 @@
+//
+//  SWCalendar.swift
+//  SwipeCalendar
+//
+//  Created by doremin on 2023/10/16.
+//
+
+import Foundation
+
+
+
+class SWCalendar {
+    let calendar: Calendar
+    
+    var days = [Date]()
+    
+    init(calendar: Calendar = .current) {
+        self.calendar = calendar
+    }
+    
+    func generateDaysOfWeek(date: Date) -> [SWDay] {
+        
+        let weekday = calendar.component(.weekday, from: date)
+        let yesterdays = (1 ..< weekday)
+            .reversed()
+            .compactMap { calendar.date(byAdding: .day, value: $0 * -1, to: date) }
+        
+        print(yesterdays)
+        
+        let futures = (0 ..< 8 - weekday)
+            .compactMap { calendar.date(byAdding: .day, value: $0, to: date) }
+        
+        let daysOfWeek = (yesterdays + futures)
+            .map {
+                SWDay(
+                    year: calendar.component(.year, from: $0),
+                    month: calendar.component(.month, from: $0),
+                    day: calendar.component(.day, from: $0))
+            }
+        
+        return daysOfWeek
+    }
+}
---
+++ SwipeCalendar/SWDay.swift
@@ -0,0 +1,32 @@
+//
+//  SWDay.swift
+//  SwipeCalendar
+//
+//  Created by doremin on 2023/10/31.
+//
+
+import Foundation
+
+class SWDay {
+    let year: Int
+    let month: Int
+    let day: Int
+    
+    init(
+        year: Int,
+        month: Int,
+        day: Int)
+    {
+        self.year = year
+        self.month = month
+        self.day = day
+    }
+}
+
+extension SWDay: Equatable {
+    static func == (lhs: SWDay, rhs: SWDay) -> Bool {
+        lhs.year == rhs.year
+        && lhs.month == rhs.month
+        && lhs.day == rhs.day
+    }
+}
